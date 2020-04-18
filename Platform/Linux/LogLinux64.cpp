7230bd6e502be4f8c41a32d86d025e9c48006dba
rmdir-dev <stonsphilippe@gmail.com>
2020-04-18 10:53:03 +0200

    Linux platform logs

---
+++ Platform/Linux/LogLinux64.cpp
@@ -0,0 +1,42 @@
+
+#if __linux__ == 1
+
+#include "Utils/Log.h"
+#include <signal.h>
+#include "Utils/pch.h"
+
+namespace Log
+{
+	void platformLogMessage(LogLevel level, const char* message)
+	{
+        //Reset the terminal colors to normal.
+		std::string reset = "\033[0m";
+        //Create a new line
+		reset += '\n';
+
+		switch (level)
+		{
+		case LogLevel::Info:
+			std::cout << "\033[1;37m" << "[INFO] " << message << reset;
+			break;
+		case LogLevel::Error:
+			std::cout << "\033[1;31m" << "[ERROR] " << message << reset;
+			break;
+		case LogLevel::Warning:
+			std::cout << "\033[1;33m" << "[WARNING] " << message << reset;
+			break;
+		case LogLevel::Critical:
+			std::cout << "\033[1;37;41m" << "[CRITICAL] " << message << reset;
+			break;
+		default:
+			break;
+		}
+	}
+
+	void platformBreak()
+	{
+        //Raise a sigtrip to break the program when it catches an assertion.
+		raise(SIGTRAP);
+	}
+}
+#endif
