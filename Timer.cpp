0b065da2a78b55ba892dfe0ba24cad39475b17a0
rmdir-dev <stonsphilippe@gmail.com>
2020-04-18 11:30:40 +0200

    Add comment + function name

--- Timer.cpp
+++ Timer.cpp
@@ -1,13 +1,21 @@
 #include "Timer.h"
 #include <iostream>
+#include "Log.h"
 
-Timer::Timer(){
+Timer::Timer(std::string fctName)
+    : fctName(fctName)
+{
+    //Start the timer (set the start variable)
     start = HrClock::now();
 }
 
 Timer::~Timer(){
+    //End the timer (set a TPoint end variable)
     TPoint end = HrClock::now();
+    //Calculate the elaspe time
     eTime elaspeTime = end - start;
+    //Convert the elaspe time to ms
     float ms = elaspeTime.count() * 1000;
-    std::cout << ms << "ms\n";
+    //Output the elaspe time.
+    CORE_INFO(fctName, " took: ", ms, "ms to execute");
 }
--- Timer.h
+++ Timer.h
@@ -1,16 +1,40 @@
 #pragma once
 #include <chrono>
+#include <string>
 
 using HrClock = std::chrono::high_resolution_clock;
 using eTime = std::chrono::duration<double>;
 using TPoint = std::chrono::high_resolution_clock::time_point;
 
+/*
+    Start the timer.
+    THE TIMER MUST NOT BE A POINTER !
+    Timer use it's life cycle to time your function, so it's better to allocate it on the stack,
+    if you do allocate it on the heap DESTROY IT!
+    
+    Args:
+    fctName = function name or the name of the part you want to debug.
+    DEFAULT : "NO NAME"
+    use __func__ to get the function name.
+*/
 class Timer {
 private:
     TPoint start;
+    std::string fctName;
 
 public:
-    Timer();
+    /*
+    Start the timer.
+    THE TIMER MUST NOT BE A POINTER !
+    Timer use it's life cycle to time your function, so it's better to allocate it on the stack,
+    if you do allocate it on the heap DESTROY IT!
+
+    Args:
+    fctName = function name or the name of the part you want to debug.
+    DEFAULT : "NO NAME"
+    use __func__ to get the function name.
+    */
+    Timer(std::string fctName = "NO NAME");
 
     ~Timer();
 };
